097cc2ba50351619ff2c150a68aec2a5048fa0cc
María A. Matienzo <matienzo@berkeley.edu>
2024-09-17 16:53:59 -0700

    add more pseudocode notes for progress

--- app/jobs/enumerate_files_job.rb
+++ app/jobs/enumerate_files_job.rb
@@ -2,9 +2,17 @@
 
 # job to enumerate files within a directory for a dataload
 class EnumerateFilesJob < ApplicationJob
+  include GoodJob::ActiveJobExtensions::Batches
   queue_as :default
 
   def perform(*args)
+    # 1. get the Dataload object
+    # 2. get the real path:
+    #   * get the :source key, and look up its :path attribute
+    #   * concat the :path + '/' + the :directory attrib from Dataload
+    # 3. create a GoodJob::Batch
+    #   * for each file, add a new job to the batch to process that file
+    #
     # realpath = Pathname.new(path).realpath
     # Pathname.glob(realpath + '**/*') do |p|
     #   Datafile.new(whatever)
