02ae922c25d2886bc9f093d24b928bd149566647
María A. Matienzo <matienzo@berkeley.edu>
2024-10-03 16:07:55 -0700

    correct call after renaming; add additional batch metadata

--- app/jobs/create_batch_for_dataload_job.rb
+++ app/jobs/create_batch_for_dataload_job.rb
@@ -8,7 +8,11 @@ class CreateBatchForDataloadJob < ApplicationJob
     dataload.update!(status: :in_progress)
     batch = GoodJob::Batch.new
     batch.properties[:dataload] = dataload
-    batch.properties[:source_path] = DataverseService::Mountpoints.new.public_send("#{dataload.mountPoint}_path")
+    batch.properties[:dataload_gid] = dataload.to_global_id.to_s
+    batch.properties[:doi] = dataload.doi
+    batch.properties[:source_path] = dataload.realpath.to_s
+    batch.description = "Dataload #{dataload.id}: source: #{dataload.realpath}; destination DOI #{dataload.doi}"
+    batch.save
 
     batch.add do
       Pathname.new(batch.properties[:source_path]).glob('**/*') do |f|
@@ -16,6 +20,6 @@ class CreateBatchForDataloadJob < ApplicationJob
       end
     end
 
-    batch.enqueue(on_success: 'CreateDatafileInDataverseCallbackJob')
+    batch.enqueue(on_success: 'CreateDatafilesInDataverseCallbackJob')
   end
 end
