47255f8caef99ac69fab99378cf03e35a4bef71f
Steve Sullivan <ssully28@gmail.com>
2024-11-12 08:57:14 -0800

    wip: add data load

--- app/controllers/data_loads_controller.rb
+++ app/controllers/data_loads_controller.rb
@@ -27,10 +27,28 @@ class DataLoadsController < ApplicationController
   end
 
   # TODO: Form to create new dataload
-  def new; end
+  def new
+    @dataload = Dataload.new
+  end
 
   # TODO: Create a new dataload!!!
-  def create; end
+  def create
+    puts "------------------------------------------"
+    puts "------------------------------------------"
+    puts "------------------------------------------"
+    puts "HEYYYYY!!!!"
+    puts "------------------------------------------"
+    puts "------------------------------------------"
+    puts "------------------------------------------"
+    @dataload = Dataload.new(dataload_params)
+
+    if @dataload.save
+      flash['notice'] = "HELLLOOOO...is this thing on????"
+      redirect_to data_loads_path
+    else
+      render :new
+    end
+  end
 
   def update
     @data_load = Dataload.find(params[:id])
@@ -40,6 +58,10 @@ class DataLoadsController < ApplicationController
   end
 
   private
+
+  def dataload_params
+    params.require(:dataload).permit(:doi, :mountPoint, :directory, :user_name, :user_email)
+  end
 
   def authorized?
     return true if signed_in? && dataverse_user?
@@ -57,3 +79,20 @@ class DataLoadsController < ApplicationController
     [pagy, data_loads]
   end
 end
+
+
+__END__
+
+Must have: A user should be able to create a new data load, which kicks off a new set of jobs.
+To create a new data load, the user must know:
+    the DOI for an existing Dataset in Dataverse (preferably in a “Draft” version)
+    the mount point / directory for the dataset’s files that have already been curated
+The user fills out the form and clicks Submit:
+    DOI: textfield 
+    Mount point: dropdown
+    Directory: textfield
+Upon clicking Submit:
+    Could have: The application calls the Dataverse API to get information about the Dataset the files will be loaded into. 
+    Must have: The user then sees a partial file listing to confirm the directory is correct.
+The user can click Save/Confirm or Cancel.
+Must have: Upon Save/Confirm, the new batch jobs are created, and the user is taken back to the application home again. A flash should appear saying that the data load has been created. 
